a89eb213868f00aa4bcb793dc488f2253705636e
Anonynet1 <116462339+Anonynet1@users.noreply.github.com>
2022-10-26 08:36:55 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -136,7 +136,7 @@ install_x-ui() {
     cd x-ui
     chmod +x x-ui bin/xray-linux-${arch}
     cp -f x-ui.service /etc/systemd/system/
-    wget --no-check-certificate -O /usr/bin/x-ui https://raw.githubusercontent.com/vaxilu/x-ui/main/x-ui.sh
+    wget --no-check-certificate -O /usr/bin/x-ui https://raw.githubusercontent.com/Anonynet1/v2ray-/main/x-ui.sh
     chmod +x /usr/local/x-ui/x-ui.sh
     chmod +x /usr/bin/x-ui
     config_after_install
