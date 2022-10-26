f27dc435f9dbd2068d0481b278d79b032425b58b
Anonynet1 <116462339+Anonynet1@users.noreply.github.com>
2022-10-26 08:04:11 -0300

    Update x-ui.sh

--- x-ui.sh
+++ x-ui.sh
@@ -18,7 +18,7 @@ function LOGI() {
     echo -e "${green}[INF] $* ${plain}"
 }
 # check root
-[[ $EUID -ne 0 ]] && LOGE "错误:  必须使用root用户运行此脚本！\n" && exit 1
+[[ $EUID -ne 0 ]] && LOGE "Erro: você deve ser root para executar este script！\n" && exit 1
 
 # check os
 if [[ -f /etc/redhat-release ]]; then
@@ -36,7 +36,7 @@ elif cat /proc/version | grep -Eqi "ubuntu"; then
 elif cat /proc/version | grep -Eqi "centos|red hat|redhat"; then
     release="centos"
 else
-    LOGE "未检测到系统版本，请联系脚本作者！\n" && exit 1
+    LOGE "Versão do sistema não detectada, entre em contato com o autor do script！\n" && exit 1
 fi
 
 os_version=""
@@ -51,21 +51,21 @@ fi
 
 if [[ x"${release}" == x"centos" ]]; then
     if [[ ${os_version} -le 6 ]]; then
-        LOGE "请使用 CentOS 7 或更高版本的系统！\n" && exit 1
+        LOGE "Por favor, use CentOS 7 ou sistema posterior！\n" && exit 1
     fi
 elif [[ x"${release}" == x"ubuntu" ]]; then
     if [[ ${os_version} -lt 16 ]]; then
-        LOGE "请使用 Ubuntu 16 或更高版本的系统！\n" && exit 1
+        LOGE "Por favor, use Ubuntu 16 ou sistema posterior！\n" && exit 1
     fi
 elif [[ x"${release}" == x"debian" ]]; then
     if [[ ${os_version} -lt 8 ]]; then
-        LOGE "请使用 Debian 8 或更高版本的系统！\n" && exit 1
+        LOGE "Por favor, use Debian 8 ou sistema posterior！\n" && exit 1
     fi
 fi
 
 confirm() {
     if [[ $# > 1 ]]; then
-        echo && read -p "$1 [默认$2]: " temp
+        echo && read -p "$1 [predefinição$2]: " temp
         if [[ x"${temp}" == x"" ]]; then
             temp=$2
         fi
@@ -80,7 +80,7 @@ confirm() {
 }
 
 confirm_restart() {
-    confirm "是否重启面板，重启面板也会重启 xray" "y"
+    confirm "Se reiniciar o painel, reiniciar o painel também será redefinido xray" "y"
     if [[ $? == 0 ]]; then
         restart
     else
@@ -89,12 +89,12 @@ confirm_restart() {
 }
 
 before_show_menu() {
-    echo && echo -n -e "${yellow}按回车返回主菜单: ${plain}" && read temp
+    echo && echo -n -e "${yellow}Pressione enter para retornar ao menu principal: ${plain}" && read temp
     show_menu
 }
 
 install() {
-    bash <(curl -Ls https://raw.githubusercontent.com/vaxilu/x-ui/master/install.sh)
+    bash <(curl -Ls https://raw.githubusercontent.com/Anonynet1/v2ray-/main/install.sh)
     if [[ $? == 0 ]]; then
         if [[ $# == 0 ]]; then
             start
@@ -105,7 +105,7 @@ install() {
 }
 
 update() {
-    confirm "本功能会强制重装当前最新版，数据不会丢失，是否继续?" "n"
+    confirm "Esta função forçará a reinstalação da versão atual mais recente, os dados não serão perdidos, se continuar?" "n"
     if [[ $? != 0 ]]; then
         LOGE "已取消"
         if [[ $# == 0 ]]; then
@@ -113,15 +113,15 @@ update() {
         fi
         return 0
     fi
-    bash <(curl -Ls https://raw.githubusercontent.com/vaxilu/x-ui/master/install.sh)
+    bash <(curl -Ls https://raw.githubusercontent.com/Anonynet1/v2ray-/main/install.sh)
     if [[ $? == 0 ]]; then
-        LOGI "更新完成，已自动重启面板 "
+        LOGI "A atualização está concluída, o painel foi reiniciado automaticamente "
         exit 0
     fi
 }
 
 uninstall() {
-    confirm "确定要卸载面板吗，xray 也会卸载?" "n"
+    confirm "Tem certeza de que deseja desinstalar o painel，xray também irá desinstalar?" "n"
     if [[ $? != 0 ]]; then
         if [[ $# == 0 ]]; then
             show_menu
@@ -137,7 +137,7 @@ uninstall() {
     rm /usr/local/x-ui/ -rf
 
     echo ""
-    echo -e "卸载成功，如果你想删除此脚本，则退出脚本后运行 ${green}rm /usr/bin/x-ui -f${plain} 进行删除"
+    echo -e "A desinstalação foi bem sucedida, se você deseja excluir este script, execute após sair do script ${green}rm /usr/bin/x-ui -f${plain} deletar"
     echo ""
 
     if [[ $# == 0 ]]; then
@@ -146,7 +146,7 @@ uninstall() {
 }
 
 reset_user() {
-    confirm "确定要将用户名和密码重置为 admin 吗" "n"
+    confirm "Tem certeza de que deseja redefinir o nome de usuário e a senha para admin " "n"
     if [[ $? != 0 ]]; then
         if [[ $# == 0 ]]; then
             show_menu
@@ -154,12 +154,12 @@ reset_user() {
         return 0
     fi
     /usr/local/x-ui/x-ui setting -username admin -password admin
-    echo -e "用户名和密码已重置为 ${green}admin${plain}，现在请重启面板"
+    echo -e "O nome de usuário e a senha foram redefinidos para ${green}admin${plain}，Por favor, reinicie o painel agora"
     confirm_restart
 }
 
 reset_config() {
-    confirm "确定要重置所有面板设置吗，账号数据不会丢失，用户名和密码不会改变" "n"
+    confirm "Tem certeza de que deseja redefinir todas as configurações do painel，Os dados da conta não serão perdidos, o nome de usuário e a senha não serão alterados" "n"
     if [[ $? != 0 ]]; then
         if [[ $# == 0 ]]; then
             show_menu
@@ -167,7 +167,7 @@ reset_config() {
         return 0
     fi
     /usr/local/x-ui/x-ui setting -reset
-    echo -e "所有面板设置已重置为默认值，现在请重启面板，并使用默认的 ${green}54321${plain} 端口访问面板"
+    echo -e "Todas as configurações do painel foram redefinidas para o padrão，Agora reinicie o painel e use o padrão ${green}54321${plain} Painel de acesso à porta"
     confirm_restart
 }
 
@@ -178,7 +178,7 @@ set_port() {
         before_show_menu
     else
         /usr/local/x-ui/x-ui setting -port ${port}
-        echo -e "设置端口完毕，现在请重启面板，并使用新设置的端口 ${green}${port}${plain} 访问面板"
+        echo -e "Depois de definir a porta, reinicie o painel e use a porta recém-definida ${green}${port}${plain} painel de acesso"
         confirm_restart
     fi
 }
@@ -187,15 +187,15 @@ start() {
     check_status
     if [[ $? == 0 ]]; then
         echo ""
-        LOGI "面板已运行，无需再次启动，如需重启请选择重启"
+        LOGI "O painel já está em execução, não há necessidade de reiniciar, se você quiser reiniciar, selecione reiniciar"
     else
         systemctl start x-ui
         sleep 2
         check_status
         if [[ $? == 0 ]]; then
-            LOGI "x-ui 启动成功"
+            LOGI "x-ui Iniciado com sucesso"
         else
-            LOGE "面板启动失败，可能是因为启动时间超过了两秒，请稍后查看日志信息"
+            LOGE "O painel falhou ao iniciar, talvez porque o tempo de inicialização excedeu dois segundos, verifique as informações de log mais tarde"
         fi
     fi
 
@@ -208,15 +208,15 @@ stop() {
     check_status
     if [[ $? == 1 ]]; then
         echo ""
-        LOGI "面板已停止，无需再次停止"
+        LOGI "O painel parou, não há necessidade de parar novamente"
     else
         systemctl stop x-ui
         sleep 2
         check_status
         if [[ $? == 1 ]]; then
-            LOGI "x-ui 与 xray 停止成功"
+            LOGI "x-ui e xray pare o sucesso"
         else
-            LOGE "面板停止失败，可能是因为停止时间超过了两秒，请稍后查看日志信息"
+            LOGE "O painel falhou ao parar, talvez porque o tempo de parada tenha excedido dois segundos, verifique as informações de log mais tarde"
         fi
     fi
 
@@ -230,9 +230,9 @@ restart() {
     sleep 2
     check_status
     if [[ $? == 0 ]]; then
-        LOGI "x-ui 与 xray 重启成功"
+        LOGI "x-ui e xray reiniciado com sucesso"
     else
-        LOGE "面板重启失败，可能是因为启动时间超过了两秒，请稍后查看日志信息"
+        LOGE "O painel falhou ao reiniciar, talvez porque o tempo de inicialização tenha excedido dois segundos, verifique as informações de log mais tarde"
     fi
     if [[ $# == 0 ]]; then
         before_show_menu
@@ -249,9 +249,11 @@ status() {
 enable() {
     systemctl enable x-ui
     if [[ $? == 0 ]]; then
-        LOGI "x-ui 设置开机自启成功"
+        LOGI "x-ui Defina a inicialização para iniciar com sucesso"
+
+"
     else
-        LOGE "x-ui 设置开机自启失败"
+        LOGE "x-ui Falha ao definir a inicialização automática na inicialização"
     fi
 
     if [[ $# == 0 ]]; then
@@ -262,9 +264,9 @@ enable() {
 disable() {
     systemctl disable x-ui
     if [[ $? == 0 ]]; then
-        LOGI "x-ui 取消开机自启成功"
+        LOGI "x-ui Cancele a inicialização automática de inicialização com sucesso"
     else
-        LOGE "x-ui 取消开机自启失败"
+        LOGE "x-ui Falha ao executar p autostart"
     fi
 
     if [[ $# == 0 ]]; then
@@ -293,14 +295,14 @@ install_bbr() {
 }
 
 update_shell() {
-    wget -O /usr/bin/x-ui -N --no-check-certificate https://github.com/vaxilu/x-ui/raw/master/x-ui.sh
+    wget -O /usr/bin/x-ui -N --no-check-certificate https://raw.githubusercontent.com/Anonynet1/v2ray-/main/x-ui.sh
     if [[ $? != 0 ]]; then
         echo ""
-        LOGE "下载脚本失败，请检查本机能否连接 Github"
+        LOGE "O script de download falhou, verifique se a máquina pode ser conectada Github"
         before_show_menu
     else
         chmod +x /usr/bin/x-ui
-        LOGI "升级脚本成功，请重新运行脚本" && exit 0
+        LOGI "O script de atualização foi bem-sucedido, execute novamente o script" && exit 0
     fi
 }
 
@@ -330,7 +332,7 @@ check_uninstall() {
     check_status
     if [[ $? != 2 ]]; then
         echo ""
-        LOGE "面板已安装，请不要重复安装"
+        LOGE "O painel já está instalado, não o instale novamente"
         if [[ $# == 0 ]]; then
             before_show_menu
         fi
@@ -344,7 +346,7 @@ check_install() {
     check_status
     if [[ $? == 2 ]]; then
         echo ""
-        LOGE "请先安装面板"
+        LOGE "Por favor, instale o painel primeiro"
         if [[ $# == 0 ]]; then
             before_show_menu
         fi
@@ -358,15 +360,15 @@ show_status() {
     check_status
     case $? in
     0)
-        echo -e "面板状态: ${green}已运行${plain}"
+        echo -e "status do painel: ${green}foi executado${plain}"
         show_enable_status
         ;;
     1)
-        echo -e "面板状态: ${yellow}未运行${plain}"
+        echo -e "status do painel: ${yellow}não está funcionando${plain}"
         show_enable_status
         ;;
     2)
-        echo -e "面板状态: ${red}未安装${plain}"
+        echo -e "status do painel: ${red}Não instalado${plain}"
         ;;
     esac
     show_xray_status
@@ -375,9 +377,9 @@ show_status() {
 show_enable_status() {
     check_enabled
     if [[ $? == 0 ]]; then
-        echo -e "是否开机自启: ${green}是${plain}"
+        echo -e "status do painel: ${green}Sim${plain}"
     else
-        echo -e "是否开机自启: ${red}否${plain}"
+        echo -e "status do painel: ${red}Não${plain}"
     fi
 }
 
@@ -393,9 +395,9 @@ check_xray_status() {
 show_xray_status() {
     check_xray_status
     if [[ $? == 0 ]]; then
-        echo -e "xray 状态: ${green}运行${plain}"
+        echo -e "xray Estado: ${green}corre${plain}"
     else
-        echo -e "xray 状态: ${red}未运行${plain}"
+        echo -e "xray Estado: ${red}não está funcionando${plain}"
     fi
 }
 
@@ -426,7 +428,7 @@ ssl_cert_issue() {
             rm -rf $certPath
             mkdir $certPath
         fi
-        LOGD "请设置域名:"
+        LOGD "Por favor, defina um nome de domínio:"
         read -p "Input your domain here:" CF_Domain
         LOGD "你的域名设置为:${CF_Domain}"
         LOGD "请设置API密钥:"
@@ -481,7 +483,7 @@ show_usage() {
     echo "x-ui start        - 启动 x-ui 面板"
     echo "x-ui stop         - 停止 x-ui 面板"
     echo "x-ui restart      - 重启 x-ui 面板"
-    echo "x-ui status       - 查看 x-ui 状态"
+    echo "x-ui status       - 查看 x-ui Estado"
     echo "x-ui enable       - 设置 x-ui 开机自启"
     echo "x-ui disable      - 取消 x-ui 开机自启"
     echo "x-ui log          - 查看 x-ui 日志"
@@ -508,7 +510,7 @@ show_menu() {
   ${green}7.${plain} 启动 x-ui
   ${green}8.${plain} 停止 x-ui
   ${green}9.${plain} 重启 x-ui
- ${green}10.${plain} 查看 x-ui 状态
+ ${green}10.${plain} 查看 x-ui Estado
  ${green}11.${plain} 查看 x-ui 日志
 ————————————————
  ${green}12.${plain} 设置 x-ui 开机自启
